06c1116605a37700261846fae404636aa200ac99
cempack <elliotmoreau19@gmail.com>
2024-01-23 11:21:17 +0100

    Installed some SEO Modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/ui']
+  modules: [
+    '@nuxt/ui',
+    '@nuxtjs/sitemap',
+    'nuxt-simple-robots'
+  ],
+  site: {
+    url: 'https://v3.journal.elliotmoreau.fr',
+  }
 })
